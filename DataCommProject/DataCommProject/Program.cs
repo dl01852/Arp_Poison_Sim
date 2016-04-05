8654ab8e4c1136216cc55cc24a0b4f13c3eea927
dl01852 <davlewis2011@gmail.com>
2016-04-05 00:06:06 -0400

    added more code in client. issue with ping function

--- DataCommProject/DataCommProject/Program.cs
+++ DataCommProject/DataCommProject/Program.cs
@@ -14,7 +14,8 @@ namespace DataCommProject
             Router R1 = new Router();
             Switch s1 = new Switch(R1);
             Client c1 = new Client(s1,"55:22:ff:22:ff:aa:");
-            c1.ping(IPAddress.Parse("124.241.55.33"));
+            Client c2 = new Client(s1,"BB:21:52:cc:68:af");
+            c1.ping(c2.IpAddress);
             Console.ReadLine();
         }
     }
